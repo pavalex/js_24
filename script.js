c75988392ad3dfc58945976b9a40cb6753f2d593
pavalex <Alex.P79@mail.ru>
2022-03-25 22:53:47 +0800

    Выполнил второе ДЗ

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 "use strict";
 
-let title;
-let screens;
-let screenPrice;
-let rollBack;
-let fullPrice;
-let adaptive;
+let title = 'My first project';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 1979;
+let rollBack = 23;
+let fullPrice = 50000;
+let adaptive = true;
 
-alert('Это сообщение в модальном окне');
-
-console.log('Это  сообщение в консоли разработчика');
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(typeof(adaptive));
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split(', '));
+console.log('Процент отката посреднику за работу: ' + fullPrice * (rollBack / 100) + ' %');
