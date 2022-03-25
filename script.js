5cab585eea235a9f57f40db79c75ac53294d5d0d
pavalex <Alex.P79@mail.ru>
2022-03-25 23:46:28 +0800

    исправление замечаний

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 "use strict";
 
-let title = 'My first project';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 1979;
-let rollBack = 23;
-let fullPrice = 50000;
-let adaptive = true;
+const title = 'My first project';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 1979;
+const rollBack = 23;
+const fullPrice = 50000;
+const adaptive = true;
 
-console.log(typeof(title));
-console.log(typeof(fullPrice));
-console.log(typeof(adaptive));
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 console.log(screens.length);
 console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
 console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 console.log(screens.toLowerCase().split(', '));
-console.log('Процент отката посреднику за работу: ' + fullPrice * (rollBack / 100) + ' %');
+console.log(`Процент отката посреднику за работу: ${fullPrice * (rollBack / 100)} рублей`);
