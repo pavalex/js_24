056d02f7cee3801a8b2d64e64d9aa3c112bdb667
pavalex <Alex.P79@mail.ru>
2022-03-25 19:18:00 +0800

    Выполнил первое ДЗ

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let title;
+let screens;
+let screenPrice;
+let rollBack;
+let fullPrice;
+let adaptive;
+
+alert('Это сообщение в модальном окне');
+
+console.log('Это  сообщение в консоли разработчика');
